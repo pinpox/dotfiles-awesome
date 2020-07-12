76bafa0fe40edc95844f710326d488eb35522c8a
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-07-12 10:19:08 +0200

    simplify menu

--- rc.lua
+++ rc.lua
@@ -109,19 +109,13 @@ local tags = sharedtags({
 -- }}}
 
 -- {{{ Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
+-- Create the main menu
+mymainmenu = awful.menu({ items = { { "edit config", editor_cmd .. " " .. awesome.conffile },
    { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
    { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
    { "restart", awesome.restart },
    { "quit", function() awesome.quit() end },
-}
-
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
-                                  }
-                        })
+} })
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                      menu = mymainmenu })
