17e0607cb97210a6e4abed093702464ba5cb20ce
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-01 23:11:26 +0100

    update flake.lock

--- flake.nix
+++ flake.nix
@@ -4,13 +4,11 @@
 
   outputs = { self, nixpkgs }: {
 
-    defaultPackage.x86_64-linux =
-      # Notice the reference to nixpkgs here.
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "awesome-dotfiles";
-        src = self;
-        installPhase = "mkdir -p $out; mv * $out";
+    nixosModules = {
+      dotfiles = {
+      home.file = {
+        ".config/awesome".source = ./
       };
+    };
   };
 }
