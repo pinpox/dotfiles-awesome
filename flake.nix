953264d01595d5c0434a214dd045fae17c5fac05
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-02 00:57:00 +0100

    nixfmt

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
   description = "A very basic flake";
-  inputs = { 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     wallpaper-generator.url = "github:pinpox/wallpaper-generator";
     wallpaper-generator.flake = false;
   };
 
-  outputs = { self, nixpkgs, wallpaper-generator}: {
+  outputs = { self, nixpkgs, wallpaper-generator }: {
 
     nixosModules = {
       dotfiles = { config, ... }: {
-        home.file = { 
-          ".config/awesome".source = ./dotfiles; 
+        home.file = {
+          ".config/awesome".source = ./dotfiles;
           ".local/share/wallpaper-generator".source = wallpaper-generator;
         };
       };
