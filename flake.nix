6a57ebc51c0edd37560fb6303b59066805ef49ac
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-02 00:36:39 +0100

    add wallpaper-generator

--- flake.nix
+++ flake.nix
@@ -1,12 +1,19 @@
 {
   description = "A very basic flake";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = { 
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
+    wallpaper-generator.url = "github:pinpox/wallpaper-generator";
+    wallpaper-generator.flake = false;
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, wallpaper-generator}: {
 
     nixosModules = {
       dotfiles = { config, ... }: {
-        home.file = { ".config/awesome".source = ./dotfiles; };
+        home.file = { 
+          ".config/awesome".source = ./dotfiles; 
+          ".config/awesome/wallpaper-generator".source = wallpaper-generator;
+        };
       };
     };
   };
