65861df9fc8c6e4e656d59e2b9274a96175200bc
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-06 19:12:23 +0100

    rounded corners

--- dotfiles/rc.lua
+++ dotfiles/rc.lua
@@ -170,6 +170,11 @@ root.keys(globalkeys)
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c)
+
+    -- Rounded corners for all clients, 5px radius
+    c.shape = function(cr,w,h)
+            gears.shape.rounded_rect(cr,w,h,5)
+    end
     -- Set the windows at the slave,
     -- i.e. put it at the end of others instead of setting it master.
     -- if not awesome.startup then awful.client.setslave(c) end
