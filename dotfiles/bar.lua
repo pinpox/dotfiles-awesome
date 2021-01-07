810e726e75f2c95fcdd30305ae1ef9d387469d4a
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-07 11:49:54 +0100

    try bar snippet

--- dotfiles/bar.lua
+++ dotfiles/bar.lua
@@ -113,9 +113,18 @@ awful.screen.connect_for_each_screen(function(s)
 	}
 
 
-	function custom_shape(cr, width, height)
-		gears.shape.transform(gears.shape.rounded_rect) : translate(10, -10) (cr, width -20, height, 5)
-	end
+	-- function custom_shape(cr, width, height)
+	-- 	gears.shape.transform(gears.shape.rounded_rect) : translate(10, -10) (cr, width -20, height, 5)
+	-- end
+
+local function custom_shape(cr, width, height)
+    local inner_shape = function(cr, width, height)
+        gears.shape.rounded_rect(cr, width -10 , height -10, 5)
+    end
+    gears.shape.transform(inner_shape)
+        : translate(10, 10)
+        (cr, width, height)
+end
 
 	-- Create the wibox
 	s.mywibox = awful.wibox({
