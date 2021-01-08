3fc38915a9af0988d3482c30f559e36d200b8867
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-08 01:42:43 +0100

    use circle function

--- dotfiles/theme.lua
+++ dotfiles/theme.lua
@@ -167,12 +167,9 @@ function squircle(size, fg, radius)
     cr:set_source(gears.color.transparent)
     cr:paint()
 
-    local width, height = size, size
-
-    local degrees = math.pi / 180.0
 
     cr:new_sub_path()
-    cr:arc(radius, radius, radius, 0 * degrees, 360 * degrees)
+    cr:circle(size/2, size/2 radius, radius)
     cr:close_path()
 
     cr:set_source(gears.color(fg))
@@ -185,21 +182,21 @@ end
 
 
 
-theme.button_red_focus = squircle(32, xrdb.color8, 12)
-theme.button_red_normal = squircle(32, xrdb.color8, 12)
-theme.button_red_hover = squircle(32, xrdb.color8, 12)
+theme.button_red_focus = squircle(24, xrdb.color8, 8)
+theme.button_red_normal = squircle(24, xrdb.color8, 8)
+theme.button_red_hover = squircle(24, xrdb.color8, 8)
 
-theme.button_yellow_focus = squircle(32, xrdb.color10, 12)
-theme.button_yellow_normal = squircle(32, xrdb.color10, 12)
-theme.button_yellow_hover = squircle(32, xrdb.color10, 12)
+theme.button_yellow_focus = squircle(24, xrdb.color10, 8)
+theme.button_yellow_normal = squircle(24, xrdb.color10, 8)
+theme.button_yellow_hover = squircle(24, xrdb.color10, 8)
 
-theme.button_green_focus = squircle(32, xrdb.color11, 12)
-theme.button_green_normal = squircle(32, xrdb.color11, 12)
-theme.button_green_hover = squircle(32, xrdb.color11, 12)
+theme.button_green_focus = squircle(24, xrdb.color11, 8)
+theme.button_green_normal = squircle(24, xrdb.color11, 8)
+theme.button_green_hover = squircle(24, xrdb.color11, 8)
 
-theme.button_blue_focus = squircle(32, xrdb.color11, 12)
-theme.button_blue_normal = squircle(32, xrdb.color11, 12)
-theme.button_blue_hover = squircle(32, xrdb.color11, 12)
+theme.button_blue_focus = squircle(24, xrdb.color11, 8)
+theme.button_blue_normal = squircle(24, xrdb.color11, 8)
+theme.button_blue_hover = squircle(24, xrdb.color11, 8)
 
 theme.titlebar_close_button_focus = theme.button_red_focus --close_button_focus.
 theme.titlebar_close_button_focus_hover = theme.button_red_focus --close_button_focus_hover.
