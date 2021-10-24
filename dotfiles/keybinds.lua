31457003f4d07d1bc505155f4a68443326558e0e
Pablo Ovelleiro Corral <github@pablo.tools>
2021-10-24 20:43:51 +0200

    add keybind for printscreen

--- dotfiles/keybinds.lua
+++ dotfiles/keybinds.lua
@@ -136,7 +136,7 @@ awful.key({ Modkey, "Shift"}, "x", function ()
 	awful.spawn("xscreensaver-command -lock")
 end, {description = "Lock Screen", group = "Other"}),
 
-awful.key({}, "Print", function ()
+awful.key({}, "119", function ()
 	awful.spawn.with_shell("import png:- | xclip -selection clipboard -t image/png")
 end, {description = "Screenshot to Clipboard", group = "Other"}),
 
